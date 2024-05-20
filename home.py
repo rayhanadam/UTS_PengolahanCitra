535e9467f710697bf4cd8ed65c74aba5788afdf2
rayhanadam <rayhanadam17@gmail.com>
2024-05-20 22:42:49 +0700

    UTS Pengolahan Citra

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Profile')
+st.subheader('Nama : M.Muammar')
+st.subheader('NIM : 312210663')
+st.subheader('Kelas : TI.22.B1')
+st.subheader('Prodi : Teknik Infomatika')
+
+st.image("./img/amar1.jpeg", caption="M.muammar", use_column_width="Always", width=200)
+
+
+st.link_button("Instagram", "https://www.instagram.com/muammar212/")
+st.link_button("Github", "https://github.com/muammarM")
